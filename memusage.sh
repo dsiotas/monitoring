c92ab0fd90c2efc64e9829098a79cfa5eb68aedc
dsiotas <d.siotas@gnomon.com.gr>
2015-04-16 19:55:16 +0300

    universalize

--- memusage.sh
+++ memusage.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
- date '+TIME:%H:%M' >> /home/jimmy/Documents/memusage
- free -m | grep Mem | awk '{print $1, $3}' >> /home/jimmy/Documents/memusage
- free -m | grep Swap | awk '{print $1, $3}' >> /home/jimmy/Documents/memusage
- echo >> /home/jimmy/Documents/memusage
+ date '+TIME:%H:%M' >> /home/username/memusage
+ free -m | grep Mem | awk '{print $1, $3}' >> /home/username/memusage
+ free -m | grep Swap | awk '{print $1, $3}' >> /home/username/memusage
+ echo >> /home/username/memusage
