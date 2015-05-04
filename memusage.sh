984009643ef0b0fd8b4acf2b9e73cb81c0fb96ae
dsiotas <d.siotas@gnomon.com.gr>
2015-05-04 13:50:26 +0300

    free memory info added

--- memusage.sh
+++ memusage.sh
@@ -3,3 +3,4 @@
  free -m | grep Mem | awk '{print $1, $3}' >> /home/username/memusage
  free -m | grep Swap | awk '{print $1, $3}' >> /home/username/memusage
  echo >> /home/username/memusage
+# echo "free:" free -m | grep Mem | awk '{print $1, $4}' >> /home/username/memusage
